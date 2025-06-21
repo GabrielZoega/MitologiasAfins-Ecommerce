20df5cd8cd7d094152fc89576ad7c5b4f078fe06
GabrielZoega <gabriel.zoega10@gmail.com>
2025-06-21 17:47:37 -0300

    Feat: adicionando verificação do libxcb_cursor
    Co-authored-by: Ana Flávia Marcondes Bomfim <AnaMBomfim@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -6,6 +6,29 @@ cleanup() {
 }
 trap cleanup EXIT 
 
+echo "Verificando dependências do sistema..."
+
+install_libxcb_cursor() {
+    if ldd /usr/bin/python3 2>/dev/null | grep -q "libxcb-cursor"; then
+        echo "libxcb-cursor já está presente."
+    else
+        echo "Tentando instalar libxcb-cursor0..."
+        if [ -f /etc/debian_version ]; then
+            sudo apt update
+            sudo apt install -y libxcb-cursor0
+        elif [ -f /etc/arch-release ]; then
+            sudo pacman -Sy --noconfirm libxcb-cursor
+        elif [ -f /etc/redhat-release ]; then
+            sudo dnf install -y xcb-util-cursor
+        else
+            echo "Sistema operacional não reconhecido. Instale libxcb-cursor manualmente se necessário."
+        fi
+    fi
+}
+
+install_libxcb_cursor
+
+
 echo "Iniciando nameServer..."
 python -m Pyro5.nameserver & 
 ns_pid=$!
@@ -14,7 +37,7 @@ echo "Iniciando servidor..."
 python servidor/src/ControladoraServidor.py & 
 srv_pid=$!
 
-sleep 2
+sleep 10
 
 echo "Iniciando cliente..."
 python cliente/src/ClienteMain.py &
